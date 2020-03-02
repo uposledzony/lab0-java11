a58809097c010566c3ac0882f0114f6b0b0980ea
uposledzony <chrustek123456789@gmail.com>
2020-03-02 17:53:41 +0100

    Corrected TailsGenerator

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,12 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList<String> listOfTails = new ArrayList<>(value.length());
+        for(int i = 0; i < value.length(); ++i){
+            listOfTails.add(value.substring(i));
+        }
+        listOfTails.add("");
+        return listOfTails;
     }
 
 }
